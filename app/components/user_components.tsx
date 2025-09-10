0ad81fce1567ae2f643124f576e90d313d4e59f4
vanoralin <phabhavarin2005b@gmail.com>
2025-09-10 23:14:22 +0700

    map fot trip page

---
+++ app/components/user_components.tsx
@@ -0,0 +1,27 @@
+interface HeaderProps {
+    role: number;
+}
+
+export default function RoleBar({ role }: HeaderProps) {
+    return (
+        <div className="flex">
+            <div className="border-2 w-35 p-2 pl-4 rounded-3xl border-theme-orange flex-start">
+                {role === 0 && (
+                    <h3 className="text-lg">
+                        <img src="/role_customer.svg" alt="Passenger" className="inline-block w-6 h-6 mr-2" />
+                        ผู้โดยสาร
+                    </h3>
+                )}
+                {role === 1 && (
+                    <h3 className="text-lg">
+                        <img src="/role_driver.svg" alt="Driver" className="inline-block w-6 h-6 mr-2" />
+                        คนขับ
+                    </h3>
+                )}
+                
+            </div>
+<img src="role_swap.svg" alt="" />
+
+        </div>
+    );
+}
--- app/customer/page.tsx
+++ app/customer/page.tsx
@@ -1,12 +1,50 @@
 "use client";
 
+
 import Header from "../components/welcome";
+import TripTodayCard from "./components/triptodaycard";
+import TripListCard from "./components/triplistcard";
+import BottomSheet from "./components/BottomSheet";
+import Link from "next/link";
 
 export default function Home() {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
+
+        <div className="bg-theme-customer h-full">
             <Header username="โมโมโกะ" role={0} />
+            <div className="px-4 mt-4">
+                <Link href="./customer/tripmap">
+                    <TripTodayCard />
+                </Link>
+
+            </div>
+           
+
+
+
+
+            <h1 className="text-2xl">ดูรายการทริป</h1>
+
+
+
+
+            {/* Trip List */}
+            <div className="px-4 mt-6">
+
+                <div className="flex gap-4 mt-2">
+                    <TripListCard
+                        title="ทริปปกติ"
+                        image="/home_car.png"
+                        bgColor="from-white to-[#F0B599]"
+                    />
+                    <TripListCard
+                        title="ทริปประจำ"
+                        image="/home_package.png"
+                        bgColor="from-white to-[#FFCADB]"
+                    />
+                </div>
+            </div>
+
         </div>
     );
 }
-
